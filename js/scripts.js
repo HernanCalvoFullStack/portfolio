a4353f14541ca722d70388f25c37b4d61a7373ba
Hernán Calvo <hernan.calvo@chediack.com.ar>
2024-08-03 18:53:36 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/* Typing animation */
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Student",
+    "Web Developer Full Stack",
+    "Web Developer Stack MERN-PERN",
+    "Web Designer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
